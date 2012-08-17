4c5e977c64582c54ae48ba938676a1e8cfd06613
Nic Ferier <nic@ferrier.me.uk>
2012-08-17 16:18:45 +0100

    dummy package to test uploading. also serves as a template for tar builds.

---
+++ marmalade-test.el
@@ -0,0 +1,9 @@
+(defun marmalade-test ()
+  (interactive)
+  (animate-sequence
+   (list
+    "this is just a test package"
+    "to test upload packaging in marmalade"
+    "it doesn't do anything interesting"
+    "seriously, just remove it.")))
+
